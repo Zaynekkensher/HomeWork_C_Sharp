8606fb026919abecad9d80fb1de749aef5049e3c
Zaynekkensher <kiyashko.v@me.com>
2023-04-11 19:22:34 +0300

    1

---
+++ HomeWork_1/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+ Console.Write("Введите первое число: "); 
+ int Number1 = int.Parse(Console.ReadLine()); 
+ Console.Write("Введите второе число: "); 
+ int Number2 = int.Parse(Console.ReadLine()); 
+ int max = 0;
+
+ if (Number1 > Number2)
+ {
+     max = Number1;
+ }
+ else
+ {
+     max = Number2;
+ }
+ Console.WriteLine("max " + max);
---
+++ HomeWork_1/Tsak_4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+ double Number1 = new Random().Next(100); 
+ Console.WriteLine("Случайное число №1: " + Number1); 
+ double Number2 = new Random().Next(100); 
+ Console.WriteLine("Случайное число №2: " + Number2); 
+ double Number3 = new Random().Next(100); 
+ Console.WriteLine("Случайное число №3: " + Number3); 
+ double MaxNumber = Number1; 
+
+ if (Number2 > MaxNumber) 
+ { 
+     MaxNumber = Number2; 
+ } 
+ if (Number3 > MaxNumber) 
+ { 
+     MaxNumber = Number3; 
+ } 
+ Console.Write("Из перпечисленных выше числе, максимальное является: " + MaxNumber);
