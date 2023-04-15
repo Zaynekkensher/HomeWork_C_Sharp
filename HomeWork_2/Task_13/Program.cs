80b11f82dd622db9bf7948de2331fd86f439d7b8
Zaynekkensher <kiyashko.v@me.com>
2023-04-15 10:57:34 +0300

    2134

--- HomeWork_2/Task_13/Program.cs
+++ HomeWork_2/Task_13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+ int number = new Random().Next(1, 100000);
+ Console.WriteLine(number);
+
+ if (number > 99)
+ {
+     string number1 = "";
+     number1 = Convert.ToString(number);
+     Console.WriteLine(number1[2]);
+ }
+ else
+ {
+     Console.WriteLine("Третьей цифры нет");
+ }
