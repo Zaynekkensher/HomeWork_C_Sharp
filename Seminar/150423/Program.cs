48c49b1de9b503480921afc5c39a64e88d819ed7
Zaynekkensher <kiyashko.v@me.com>
2023-04-15 14:58:35 +0300

    2451

---
+++ Seminar/150423/Program.cs
@@ -0,0 +1,25 @@
+﻿int EnterData(string text)
+{
+    Console.WriteLine(text);
+    return (int.Parse(Console.ReadLine()));
+}
+
+int x = EnterData("Введите значение x: ");
+int y = EnterData("Введите значение y: ");
+
+if (x>0 & y>0)
+{
+    Console.WriteLine("1");
+}
+if (x<0 & y>0)
+{
+    Console.WriteLine("2");
+}
+if (x<0 & y<0)
+{
+    Console.WriteLine("3");
+}
+if (x>0 & y<0)
+{
+    Console.WriteLine("4");
+}
