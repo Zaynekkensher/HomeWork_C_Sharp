5ea1ecbc6e462d1984ab219170dde12d48a2e9bc
Zaynekkensher <kiyashko.v@me.com>
2023-04-18 13:29:07 +0300

    12341

--- HomeWork_3/Task19/Program.cs
+++ HomeWork_3/Task19/Program.cs
@@ -1,9 +1,9 @@
 ﻿int num,r,sum=0,t;
 Console.Write("Введите пятизначное число: ");
 num = int.Parse(Console.ReadLine());
-if (num>9999 && num<100000)
+if (num > 9999 && num < 100000)
 {
-for(t=num;num!=0;num=num/10)
+for(t=num; num!=0; num = num/10)
 {
 r = num % 10;
 sum = sum*10+r;
