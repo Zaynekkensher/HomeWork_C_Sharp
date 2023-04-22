bf7e5ae7f667c780ba015b609ea1d77ec3a4a1c3
Zaynekkensher <kiyashko.v@me.com>
2023-04-22 13:09:44 +0300

    fdf

---
+++ HomeWork_4/Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SummDigits (string number)
+{     
+   int summ = 0;
+   
+   for(int i=0;i < number.Length; i++)
+   {
+       summ = summ + Convert.ToInt32(Char.GetNumericValue(number[i]));
+   }
+   return summ;
+}
+
+Console.WriteLine("Enter number...");
+int SumNumber = SummDigits(Console.ReadLine());
+Console.WriteLine($"Summ of digits of this number is {SumNumber}");
