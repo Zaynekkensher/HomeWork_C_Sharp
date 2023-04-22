5edcdb2b01c8c7ac4c31dbfac1aafb6d38aa2a2e
Zaynekkensher <kiyashko.v@me.com>
2023-04-22 13:02:13 +0300

    Your short summary about the commit

---
+++ HomeWork_4/Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void Progressive()
+{
+    Console.WriteLine("Введите A");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите B");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    double result = numberA;
+   for(int i=1; i<= numberB;i++)
+   {
+       result = Math.Pow(result, numberB);
+       Console.WriteLine(result);
+   }
+//   Console.WriteLine(result);
+}
+Progressive();
