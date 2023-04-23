6161a26f48bdb4c3bd675ad1fb1bde6801e17e16
Zaynekkensher <kiyashko.v@me.com>
2023-04-23 17:34:09 +0300

    435

--- HomeWork5/Task36/Program.cs
+++ HomeWork5/Task36/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[10];
+
+void NotEvenPosArray (int[] arr)
+{
+    int SummPozIndArray = 0;
+    for(int i = 0; i < array.Length; i++)
+  {
+     arr [i]=new Random().Next(15);
+      if(i % 2 == 0)
+      {
+        SummPozIndArray+=arr[i];
+      }    
+  }
+
+ Console.WriteLine(string.Join(",", array));
+ Console.WriteLine(SummPozIndArray);
+}
+NotEvenPosArray(array);
---
+++ HomeWork5/Task38/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+double[] array = new double[15];
+void MinMaxArray(double[] arr)
+{
+  double MinNum= 0;
+  double MaxNum= 0;
+  
+  for(int i = 0; i < arr.Length; i++)
+ {
+    arr[i] = new Random().Next(-500,2500);
+    Console.Write(arr[i]+" ");
+ }
+   for (int i = 1; i < arr.Length; i++)
+ {
+    if (arr[i] < MinNum) MinNum = arr[i];
+    if (arr[i] > MaxNum) MaxNum = arr[i];
+     
+ }
+     Console.WriteLine($"\nМинимальное число массива {MinNum}\nМаксимальное число массива {MaxNum}");
+     Console.WriteLine($"Разница между максимальным и минимальным числом равна {MaxNum - MinNum}");
+}
+MinMaxArray(array);
