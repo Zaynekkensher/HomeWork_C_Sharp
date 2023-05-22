0c56f2b9038c4ec8d277a846aacd0e97c8e548a5
Zaynekkensher <kiyashko.v@me.com>
2023-05-22 11:37:10 +0300

    3254

--- HomeWork7/Task68/Program.cs
+++ HomeWork7/Task68/Program.cs
@@ -1,22 +1,16 @@
-﻿Console.Clear();
+﻿Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
-long m = EnterNumbers("Enter m: ");
-long  n = EnterNumbers("Enter n: ");
+int Akkerman = A(m, n);
 
-long  functionAkkerman = Ack(m, n);
+Console.Write($"A(m,n) = {Akkerman} ");
 
-Console.Write($"Function the Akkerman = {functionAkkerman} ");
-
-long  Ack(long  m, long n)
+int A(int m, int n)
 {
   if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
-}
-
-long EnterNumbers(string input) 
-{
-  Console.Write(input);
-  long  output = Convert.ToInt64(Console.ReadLine());
-  return output;
+  else if ((m > 0) && (n == 0)) return A(m - 1, 1);
+  else return (A(m - 1, A(m, n - 1)));
 }
